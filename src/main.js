37634450b99e61592e49479e317a315aa0b0aa48
David Leach <davidleach724@gmail.com>
2021-06-06 20:15:11 -0600

    Clean up code spacing

--- src/main.js
+++ src/main.js
@@ -2,23 +2,22 @@
 //inputs
 var inputTitle = document.querySelector('#userTitle');
 var inputDescription = document.querySelector('#userBody');
-var inputSearchField = document.querySelector('#searchField')
+var inputSearchField = document.querySelector('#searchField');
 var ideaBox = document.querySelector('.container-top');
 var ideaCard = document.querySelector('#ideaCard');
 
 //buttons
 var buttonSave = document.querySelector('#saveButton');
 
-
 //event listeners
 window.addEventListener('load', retrieveFromStorage);
-buttonSave.addEventListener('click', function(event) {
+buttonSave.addEventListener('click', function (event) {
   generateIdeaCard(event);
 });
 
 ideaBox.addEventListener('keyup', buttonValidity);
 
-ideaCard.addEventListener('click', function(event) {
+ideaCard.addEventListener('click', function (event) {
   favoriteIdeaCard(event);
   deleteIdeaCard(event);
 });
@@ -30,7 +29,7 @@ var ideaCardListMain = [];
 
 //functions
 function buttonValidity() {
-  if(!inputTitle.value || !inputDescription.value) {
+  if (!inputTitle.value || !inputDescription.value) {
     buttonSave.disabled = true;
   } else if (inputTitle.value && inputDescription.value) {
     buttonSave.classList.add('save-button-2');
@@ -51,21 +50,20 @@ function generateIdeaCard(event) {
 }
 
 function renderIdeaCard(cards) {
-
   ideaCard.innerHTML = '';
 
-  for(var i=0; i < cards.length; i++) {
-  ideaCard.innerHTML += `<article id="${cards[i].id}" class="idea-card">
-    <nav class="idea-nav">
-      <img class="card-icon" id="favoriteIcon" src="${cards[i].image}" alt="favorite idea star">
-      <img class="card-icon" id="deleteIcon" src="assets/delete.svg" alt="delete idea icon">
+  for (var i = 0; i < cards.length; i++) {
+    ideaCard.innerHTML += `<article id='${cards[i].id}' class='idea-card'>
+    <nav class='idea-nav'>
+      <img class='card-icon' id='favoriteIcon' src='${cards[i].image}' alt='favorite idea star'>
+      <img class='card-icon' id='deleteIcon' src='assets/delete.svg' alt='delete idea icon'>
     </nav>
-    <div class="idea-content">
-      <h2 class="idea-title">${cards[i].title}</h2>
-      <p class="idea-body">${cards[i].body}</p>
+    <div class='idea-content'>
+      <h2 class='idea-title'>${cards[i].title}</h2>
+      <p class='idea-body'>${cards[i].body}</p>
     </div>
-    <footer class="idea-comment">
-      <img class="card-icon" src="assets/comment.svg" alt="add comment button">
+    <footer class='idea-comment'>
+      <img class='card-icon' src='assets/comment.svg' alt='add comment button'>
       <label>Comment</label>
     </footer>
   </article>`;
@@ -80,13 +78,11 @@ function clearInputFields() {
   buttonValidity();
 }
 
-
 function deleteIdeaCard(event) {
   var closestIdea = event.target.closest('article');
   if (event.target.id === 'deleteIcon') {
     for (var i = 0; i < ideaCardListMain.length; i++) {
       if (ideaCardListMain[i].id === Number(closestIdea.id)) {
-        console.log(ideaCardListMain[i]);
         ideaCardListMain[i].deleteFromStorage();
         ideaCardListMain.splice(i, 1);
       }
@@ -109,9 +105,13 @@ function favoriteIdeaCard(event) {
 function searchIdea() {
   var inputSearch = inputSearchField.value;
   var filteredIdeaCards = [];
+  console.log(inputSearch);
 
-  for (var i=0; i < ideaCardListMain.length; i++) {
-    if(ideaCardListMain[i].title.includes(inputSearch) || ideaCardListMain[i].body.includes(inputSearch)) {
+  for (var i = 0; i < ideaCardListMain.length; i++) {
+    if (
+      ideaCardListMain[i].title.includes(inputSearch) ||
+      ideaCardListMain[i].body.includes(inputSearch)
+    ) {
       filteredIdeaCards.push(ideaCardListMain[i]);
     }
   }
@@ -121,10 +121,16 @@ function searchIdea() {
 function retrieveFromStorage() {
   var keys = Object.keys(localStorage);
 
-  for (var i=0; i < keys.length; i++) {
+  for (var i = 0; i < keys.length; i++) {
     var cardDeets = localStorage.getItem(keys[i]);
     var cardDeetsObj = JSON.parse(cardDeets);
-    var newCard = new Idea(cardDeetsObj.title, cardDeetsObj.body, cardDeetsObj.id, cardDeetsObj.star, cardDeetsObj.image);
+    var newCard = new Idea(
+      cardDeetsObj.title,
+      cardDeetsObj.body,
+      cardDeetsObj.id,
+      cardDeetsObj.star,
+      cardDeetsObj.image
+    );
     ideaCardListMain.push(newCard);
   }
   renderIdeaCard(ideaCardListMain);
